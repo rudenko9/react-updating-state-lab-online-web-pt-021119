b71871f6d70cd849487c926ad7c5d11e3c0d77cb
Nataliya R <n.rudenko@gmail.com>
2019-11-14 19:12:19 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -2,24 +2,11 @@
 
 import React from 'react'
 class YouTubeDebugger extends Ract.Component {
-constructor() {
-  super();
-
-  this.state ={
-    timesClicked: 0
-  }
-}
-
-handleClick = () => {
-  let newCount = this.state.timesClicked + 1
-  this.setState({
-    timesClicked: newCount
-  })
-}
 
 
   render() {
     return(
+
       <p> </p>
     )
   }
