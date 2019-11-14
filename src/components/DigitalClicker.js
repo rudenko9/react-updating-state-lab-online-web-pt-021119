a2c4e1806c854157916f01c76fda10c32023090e
Nataliya R <n.rudenko@gmail.com>
2019-11-14 19:12:27 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -2,6 +2,21 @@
 
 import React from 'react'
 class DigitalClicker extends React.Component {
+  constructor() {
+    super();
+
+    this.state ={
+      timesClicked: 0
+    }
+  }
+
+  handleClick = () => {
+    let newCount = this.state.timesClicked + 1
+    this.setState({
+      timesClicked: newCount
+    })
+  }
+
   render() {
     return(
       <div> </div>
